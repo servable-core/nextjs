fad78f323b13bb23c4986661e9307f96efd717c4
Aboubacar Doucoure <yelounak@gmail.com>
2024-08-05 15:29:21 +0200

    fix: hooks name

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import * as Routes from './routes/index.js'
 import * as User from './user/index.js'
-import * as Hooks from './hooks/index.js'
+import * as hooks from './hooks/index.js'
 
 export default {
   Routes,
   User,
-  Hooks
+  hooks
 }
