ad4c98428a5b91edcff027c1954cc185c46be677
Aboubacar Doucoure <yelounak@gmail.com>
2024-08-05 14:46:33 +0200

    feat: fix routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import * as Routes from './routes'
 import * as User from './user'
-import * as Hooks from './hooks'
+// import * as Hooks from './hooks'
 
 export default {
-    Routes,
-    User,
-    Hooks
+  Routes,
+  User,
+  // Hooks
 }
